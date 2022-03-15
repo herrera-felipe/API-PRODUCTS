12a71b9fa5629f51c255044ca45c2d4106e8f112
Felipe Herrera <herrerafelipe.093@gmail.com>
2022-03-15 20:15:55 -0300

    Added repositories

--- src/main/java/com/challenge/apiproducts/data/repository/ProductRepository.java
+++ src/main/java/com/challenge/apiproducts/data/repository/ProductRepository.java
@@ -1,4 +1,9 @@
 package com.challenge.apiproducts.data.repository;
 
-public interface ProductRepository {
+import com.challenge.apiproducts.data.entity.ProductEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductEntity,Long> {
 }
--- src/main/java/com/challenge/apiproducts/data/repository/UserRepository.java
+++ src/main/java/com/challenge/apiproducts/data/repository/UserRepository.java
@@ -1,4 +1,13 @@
 package com.challenge.apiproducts.data.repository;
 
-public interface UserRepository {
+import com.challenge.apiproducts.data.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    UserEntity findByUserName(String username);
+    UserEntity findByEmail(String email);
+    boolean existsByEmail(String email);
 }

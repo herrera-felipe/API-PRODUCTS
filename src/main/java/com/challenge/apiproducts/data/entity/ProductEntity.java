4e54ca6f6221b9d1411f00b042e9e6dde355b599
Felipe Herrera <herrerafelipe.093@gmail.com>
2022-03-15 19:40:09 -0300

    ProductEntity created

--- src/main/java/com/challenge/apiproducts/data/entity/ProductEntity.java
+++ src/main/java/com/challenge/apiproducts/data/entity/ProductEntity.java
@@ -1,4 +1,38 @@
 package com.challenge.apiproducts.data.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "product")
 public class ProductEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private Long SKU;
+
+    @Column(nullable = false, unique = true)
+    private Long code;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    private String picture;
+
+    private Double price;
+
+    private String currency;
 }

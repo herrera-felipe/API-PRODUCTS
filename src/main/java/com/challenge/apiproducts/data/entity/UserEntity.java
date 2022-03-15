0cacb76c9ea1e49a78bc80b349d54dbd9dec0975
Felipe Herrera <herrerafelipe.093@gmail.com>
2022-03-15 20:14:29 -0300

    UserEntity created

--- src/main/java/com/challenge/apiproducts/data/entity/UserEntity.java
+++ src/main/java/com/challenge/apiproducts/data/entity/UserEntity.java
@@ -1,4 +1,44 @@
 package com.challenge.apiproducts.data.entity;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "user")
 public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate created;
+
+    private String email;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    private String password;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate updated;
+
+    private String username;
+
+    private String role;
 }

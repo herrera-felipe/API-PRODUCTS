ebc3c57a8909bbe87fa5a91c14e5738be7e2cef6
Felipe Herrera <herrerafelipe.093@gmail.com>
2022-03-16 18:16:08 -0300

    Added methods

--- src/main/java/com/challenge/apiproducts/domain/users/UserGateway.java
+++ src/main/java/com/challenge/apiproducts/domain/users/UserGateway.java
@@ -1,2 +1,13 @@
-package com.challenge.apiproducts.domain.users;public interface UserGateway {
+package com.challenge.apiproducts.domain.users;
+
+import java.util.List;
+
+public interface UserGateway {
+
+    UserModel save(UserModel userModel);
+    RoleModel saveRole(RoleModel roleModel);
+    void addRoleToUser(String username, String roleName);
+    UserModel findByUsername(String username);
+    List<UserModel> findAll();
+    UserModel update(long id, UserModel userModel);
 }

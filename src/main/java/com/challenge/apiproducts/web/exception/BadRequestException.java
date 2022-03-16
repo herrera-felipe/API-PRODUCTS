316ff2d7285e49942ee8199394bd6ace42e8523d
Felipe Herrera <herrerafelipe.093@gmail.com>
2022-03-16 18:12:37 -0300

    Added Exceptions

--- src/main/java/com/challenge/apiproducts/web/exception/BadRequestException.java
+++ src/main/java/com/challenge/apiproducts/web/exception/BadRequestException.java
@@ -1,2 +1,8 @@
-package com.challenge.apiproducts.web.exception;public class BadRequestException {
+package com.challenge.apiproducts.web.exception;
+
+public class BadRequestException extends RuntimeException {
+
+    public BadRequestException(String message) {
+        super(message);
+    }
 }
--- src/main/java/com/challenge/apiproducts/web/exception/ResourceNotFoundException.java
+++ src/main/java/com/challenge/apiproducts/web/exception/ResourceNotFoundException.java
@@ -1,2 +1,8 @@
-package com.challenge.apiproducts.web.exception;public class ResourceNotFoundException {
+package com.challenge.apiproducts.web.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }

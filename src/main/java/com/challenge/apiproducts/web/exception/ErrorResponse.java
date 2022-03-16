a734071b8aff785ab8116aa0c8e87f8c1f815107
Felipe Herrera <herrerafelipe.093@gmail.com>
2022-03-16 18:13:12 -0300

    Added Exception handler y error response

--- src/main/java/com/challenge/apiproducts/web/exception/ErrorResponse.java
+++ src/main/java/com/challenge/apiproducts/web/exception/ErrorResponse.java
@@ -1,2 +1,19 @@
-package com.challenge.apiproducts.web.exception;public class ErrorResponse {
+package com.challenge.apiproducts.web.exception;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Setter
+public class ErrorResponse {
+    private int status;
+    private String message;
+    private String uri;
+
+    public ErrorResponse(HttpStatus status, String message, String uri) {
+        this.status = status.value();
+        this.message = message;
+        this.uri = uri;
+    }
 }
--- src/main/java/com/challenge/apiproducts/web/exception/GlobalExceptionHandler.java
+++ src/main/java/com/challenge/apiproducts/web/exception/GlobalExceptionHandler.java
@@ -1,2 +1,27 @@
-package com.challenge.apiproducts.web.exception;public class GlobalExceptionHandler {
+package com.challenge.apiproducts.web.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler({ResourceNotFoundException.class})
+    @ResponseBody
+    public ErrorResponse resourceNotFound(HttpServletRequest request, Exception e) {
+        return new ErrorResponse(HttpStatus.NOT_FOUND, e.getMessage(), request.getRequestURI());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({BadRequestException.class})
+    @ResponseBody
+    public ErrorResponse badRequest(HttpServletRequest request, Exception e) {
+        return new ErrorResponse(HttpStatus.BAD_REQUEST, e.getMessage(), request.getRequestURI());
+    }
 }

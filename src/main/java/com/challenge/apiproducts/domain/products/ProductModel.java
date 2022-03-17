9ad5843aaeab553b8e1f76949496613c8bacd73a
Felipe Herrera <herrerafelipe.093@gmail.com>
2022-03-17 16:46:49 -0300

    Create model for product

--- src/main/java/com/challenge/apiproducts/domain/products/ProductModel.java
+++ src/main/java/com/challenge/apiproducts/domain/products/ProductModel.java
@@ -8,7 +8,7 @@ import lombok.Setter;
 @Setter
 @Builder
 public class ProductModel {
-    private Integer id;
+    private Long id;
     private Long SKU;
     private Integer code;
     private String name;

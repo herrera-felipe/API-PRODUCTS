e3431ee4604b5dd6f2078ce991a189dafdbd6fa2
Felipe Herrera <herrerafelipe.093@gmail.com>
2022-03-17 16:45:30 -0300

    modified repo

--- src/main/java/com/challenge/apiproducts/data/repository/ProductRepository.java
+++ src/main/java/com/challenge/apiproducts/data/repository/ProductRepository.java
@@ -4,7 +4,9 @@ import com.challenge.apiproducts.data.entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity,Long> {
-    ProductEntity findByName(String name);
+    Optional<ProductEntity> findByName(String name);
 }

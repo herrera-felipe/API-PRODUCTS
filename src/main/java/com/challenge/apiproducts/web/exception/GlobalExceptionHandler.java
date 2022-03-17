4b5595a3f7a7099d6485a3d6e09648af9ed8b466
Felipe Herrera <herrerafelipe.093@gmail.com>
2022-03-17 16:50:11 -0300

    Modified exceptionHandler

--- src/main/java/com/challenge/apiproducts/web/exception/GlobalExceptionHandler.java
+++ src/main/java/com/challenge/apiproducts/web/exception/GlobalExceptionHandler.java
@@ -11,17 +11,26 @@ import javax.servlet.http.HttpServletRequest;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({ BadRequestException.class,
+            org.hibernate.PropertyValueException.class,
+            org.springframework.dao.DataIntegrityViolationException.class,
+            org.springframework.web.HttpRequestMethodNotSupportedException.class,
+            org.springframework.web.bind.MethodArgumentNotValidException.class,
+            org.springframework.web.bind.MissingRequestHeaderException.class,
+            org.springframework.web.bind.MissingServletRequestParameterException.class,
+            org.springframework.web.method.annotation.MethodArgumentTypeMismatchException.class,
+            org.springframework.http.converter.HttpMessageNotReadableException.class
+    })
+    @ResponseBody
+    public ErrorResponse badRequest(HttpServletRequest request, Exception e){
+        return new ErrorResponse(HttpStatus.BAD_REQUEST, e.getMessage(), request.getRequestURI());
+    }
+
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler({ResourceNotFoundException.class})
     @ResponseBody
     public ErrorResponse resourceNotFound(HttpServletRequest request, Exception e) {
         return new ErrorResponse(HttpStatus.NOT_FOUND, e.getMessage(), request.getRequestURI());
-    }
-
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler({BadRequestException.class})
-    @ResponseBody
-    public ErrorResponse badRequest(HttpServletRequest request, Exception e) {
-        return new ErrorResponse(HttpStatus.BAD_REQUEST, e.getMessage(), request.getRequestURI());
     }
 }
